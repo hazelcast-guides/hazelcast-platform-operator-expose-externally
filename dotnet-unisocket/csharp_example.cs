d0313c48eebbfa5280bdaca1bdb598a98979a19c
omcal <omerisildak0@gmail.com>
2023-08-02 17:22:59 +0300

    dotnet example and git repo clone added

---
+++ dotnet-unisocket/csharp_example.cs
@@ -0,0 +1,41 @@
+﻿using Hazelcast;
+
+
+class csharp_example
+{
+    static async Task Main(string[] args)
+    {
+        IHazelcastClient client;
+        Action<HazelcastOptions> configureOptions = options =>
+        {
+            options.Networking.SmartRouting = false;
+            options.Networking.Addresses.Add("<EXTERNAL-IP>");
+            options.Networking.ConnectionRetry.ClusterConnectionTimeoutMilliseconds = 1000;
+        };
+
+        var options = new HazelcastOptionsBuilder().With(configureOptions).Build();
+        
+        try 
+        {
+            client = await HazelcastClientFactory.StartNewClientAsync(options);
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine("Failed to connect: " + e.Message);
+            return;
+        }
+
+
+        Console.WriteLine("Successful connection!");
+        Console.WriteLine("Starting to fill the map with random entries.");
+
+        var map = await client.GetMapAsync<string, string>("map");
+        Random random = new Random();
+        while (true)
+        {
+            int randomKey = random.Next(100_000);
+            await map.PutAsync("key-" + randomKey, "value-" + randomKey);
+            Console.WriteLine("Current map size: " + await map.GetSizeAsync());
+        }
+    }
+}
---
+++ dotnet/Program.cs
@@ -0,0 +1,40 @@
+﻿using Hazelcast;
+
+class csharp_example
+{
+    static async Task Main(string[] args)
+    {
+        IHazelcastClient client;
+        Action<HazelcastOptions> configureOptions = options =>
+        {
+            options.Networking.Addresses.Add("<EXTERNAL-IP>");
+            options.Networking.ConnectionRetry.ClusterConnectionTimeoutMilliseconds = 1000;
+            options.Networking.UsePublicAddresses = true;
+        };
+
+        var options = new HazelcastOptionsBuilder().With(configureOptions).Build();
+        
+        try 
+        {
+            client = await HazelcastClientFactory.StartNewClientAsync(options);
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine("Failed to connect: " + e.Message);
+            return;
+        }
+
+
+        Console.WriteLine("Successful connection!");
+        Console.WriteLine("Starting to fill the map with random entries.");
+
+        var map = await client.GetMapAsync<string, string>("map");
+        Random random = new Random();
+        while (true)
+        {
+            int randomKey = random.Next(100_000);
+            await map.PutAsync("key-" + randomKey, "value-" + randomKey);
+            Console.WriteLine("Current map size: " + await map.GetSizeAsync());
+        }
+    }
+}
